707b946f4bff826c553377fda333164bf2929f41
Ralyton <ton_trompa07@hotmail.com>
2023-03-03 22:15:45 -0300

    atualizei ex modulo 21

--- ex-modulo21/src/scripts/main.js
+++ ex-modulo21/src/scripts/main.js
@@ -6,9 +6,9 @@ document.addEventListener('DOMContentLoaded', function(){
             const abaAlvo = botao.target.dataset.tabButton;
             const aba = document.querySelector(`[data-tab-id=${abaAlvo}]`)
             escondeTodasAbas()
-            aba.classList.add('container__shows__list--is--active')
+            aba.classList.add('container__shows__list--is-active')
             removeBotaoAtivo()
-            botao.target.classList.add('container__shows__tabs__button--is--active')
+            botao.target.classList.add('container__shows__tabs__button--is-active')
         })
     }
 })
@@ -17,7 +17,7 @@ function removeBotaoAtivo() {
     const buttons = document.querySelectorAll('[data-tab-button]')
     
     for (let i = 0; i < buttons.length; i++) {
-        buttons[i].classList.remove('container__shows__tabs__button--is--active')
+        buttons[i].classList.remove('container__shows__tabs__button--is-active')
     }
 }
 
@@ -25,6 +25,6 @@ function escondeTodasAbas() {
     const tabsContainer = document.querySelectorAll('[data-tab-id]')
 
     for (let i = 0; i < tabsContainer.length; i++) {
-        tabsContainer[i].classList.remove('container__shows__list--is--active')
+        tabsContainer[i].classList.remove('container__shows__list--is-active')
     }
 }
